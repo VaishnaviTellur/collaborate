1be3537c3ac9dd62c55ba7aefb397e1f85c4592c
radhamaniBG <9148.radha@gmail.com>
2025-10-27 08:47:44 +0530

    changed

--- list.c
+++ list.c
@@ -5,12 +5,14 @@ int main()
     char arr[n];
     printf("Enter no.of subjects");
     scanf("% d", &n);
+
     printf("Enter the subjects");
     for (int i = 0; i < n; i++)
     {
         scanf("%s", arr[i]);
     }
     printf("The subjects are");
+    //new line added
     for (int i = 0; i < n; i++)
     {
         printf("%s", arr[i]);
