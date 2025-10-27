e481ac2aa2ef6c4b77753eb4ad0e37323dad4b02
VaishnaviTellur <vaishnaviit06@gmail.com>
2025-10-27 09:25:27 +0530

    Added

---
+++ list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    char arr[n];
+    printf("Enter no.of subjects");
+    scanf("% d", &n);
+    printf("Enter the subjects");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", arr[i]);
+    }
+    printf("The subjects are");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s", arr[i]);
+    }
+}
